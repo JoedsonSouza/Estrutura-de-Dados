2510f23a9db81d3b652a552c6dce4dfaedce43ec
Joedson Souza <89669216+JoedsonSouza@users.noreply.github.com>
2023-03-24 22:23:53 -0300

    Menos trabalho

--- PROVA/Q1/Q1.py
+++ PROVA/Q1/Q1.py
@@ -1,10 +1,16 @@
-def insertionSort(L):
+def insertionSort(L, L2, L3, L4):
     for i in range(1, len(L)):
         j = 0
         while(L[i]>L[j]) and (j<i):
               j = j+1
         val = L.pop(i)
+        val2 = L2.pop(i)
+        val3 = L3.pop(i)
+        val4 = L4.pop(i)
         L.insert(j, val)
+        L2.insert(j, val2)
+        L3.insert(j, val3)
+        L4.insert(j, val4)
 
 def quickSort(L, ini, fim):
     if(fim>ini):
@@ -26,7 +32,6 @@ def partition(L, ini, fim):
 def linha(L, L1, x):
     for i in range(x, len(L), 4):
         L1.append(L[i])
-        
 def qtdMedalhas(L, L1, x):
     for i in range(x, len(L), 4):
         L1.append(int(L[i]))
@@ -36,38 +41,21 @@ arquivo = open(nomeArquivo + '.txt', 'r')
 L = []
 nome = []
 ouro = []
-ouro2 = []
 prata = []
 bronze = []
-
 for i in arquivo:
     L.append(i)
 linha(L, nome, 0)
 qtdMedalhas(L, ouro, 1)
-qtdMedalhas(L, ouro2, 1)
 qtdMedalhas(L, prata, 2)
 qtdMedalhas(L, bronze, 3)
 
 maiorOuro = max(ouro)   #identifica qual o maior valor
 pMaiorOuro = ouro.index(maiorOuro)  #verifica a posição do maior valor dentro da lista ouro.
 
-Ordenado = []
-nomeOrd = []
-prataOrd = []
-bronzeOrd = []
-
 if ouro.count(maiorOuro) == 1:   #se o maior valor aparecer apenas 1 vez, já temos um campeão. Se não, é preciso utilizar outra forma de validar a vitória 
-    insertionSort(ouro2)
-    for i in range(0, len(ouro2)):
-        pos = ouro.index(ouro2[i])   #verifica qual a posição do valor atual na lista original
-        nomeOrd.append(nome[pos])   #adiciona o nome referente ao valor da medalha na nova lista ordenada
-        prataOrd.append(prata[pos])
-        bronzeOrd.append(bronze[pos])
-else:
-    #preciso verificar apenas dentre os valores emapatados em med de ouro, qual tem a maior qtd de medalhas de prata.
-    
-print(nomeOrd)
-print(ouro2)
-print(prataOrd)
-print(bronzeOrd)
-
+    insertionSort(ouro, nome, prata, bronze)
+print(nome)
+print(ouro)
+print(prata)
+print(bronze)
